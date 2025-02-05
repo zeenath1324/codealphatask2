924cf7e1282f9cb53e82f65e48ea4c5c35017cc0
zeenath1324 <zeenathnisha2411@gmail.com>
2025-02-05 11:02:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
